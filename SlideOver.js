1b03c237ad4c71c1b234f6f4c693077f89abeeab
Glenn Wedin <glenn.wedin@capgemini.com>
2016-07-01 12:01:26 +0200

    node-module

---
+++ SlideOver.js
@@ -0,0 +1,68 @@
+var Slideover = function (el) {
+  this.el = el;
+
+  //Generate html
+  let comparison = document.createElement('div'),
+      figure     = document.createElement('figure'),
+      divisor     = document.createElement('div'),
+      dragger     = document.createElement('div');
+
+      comparison.id = "comparison";
+      divisor.id = "divisor";
+      dragger.className = "dragger";
+
+      divisor.appendChild(dragger);
+      figure.appendChild(divisor);
+      comparison.appendChild(figure)
+
+      this.el.appendChild(comparison);
+
+    //Set events
+    window.onload = function() {
+      maxwidth = comparison.offsetWidth;
+
+      //Mousedrag
+      dragger.addEventListener('mousedown', function (e) {
+        //moveDivisor(e);
+        document.body.addEventListener('mousemove', moveDivisor);
+      });
+      document.body.addEventListener('mouseup', function (e) {
+        document.body.removeEventListener('mousemove', moveDivisor);
+      });
+
+      //Touch
+      dragger.addEventListener('touchstart', function (e) {
+        //moveDivisor(e);
+        document.body.addEventListener('touchmove', moveDivisor);
+      });
+      document.body.addEventListener('touchend', function (e) {
+        document.body.removeEventListener('touchmove', moveDivisor);
+      });
+
+      //window resize
+      document.body.addEventListener('resize', function () {
+        //reset
+      });
+
+      divisor.style.width = pos + "%";
+      comparison.style.opacity = "1";
+    };
+}
+
+Slideover.prototype.moveDivisor = function (e) {
+  if(e.touches) {
+    pos = (e.touches[0].clientX-4)/maxwidth*100;
+  } else {
+    pos = (e.clientX-4)/maxwidth*100;
+  }
+  if(pos > 100) { pos = 100 }
+  if(pos < 0) { pos = 0 }
+  divisor.style.width = pos + "%";
+}
+
+module.exports = Slideover;
+
+/*
+var Slideover = require('slideover');
+var s = new Slideover(el);
+*/

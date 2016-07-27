09281956b04f388eb137be9787246d0f1b1a5509
Glenn Wedin <glenn.wedin@capgemini.com>
2016-07-27 15:44:51 +0200

    minor casing fix for npm

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: "./SlideOver.js",
     output: {
         path: __dirname,
-        filename: "./dist/SlideOver.js",
+        filename: "./dist/slideover.js",
         libraryTarget: "umd"
     },
     module: {
